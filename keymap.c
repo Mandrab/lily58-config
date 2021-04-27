5f889f938026e817a3d49d737d11ae153342f287
Paolo Baldini <paolobaldini01@gmail.com>
2021-04-27 12:25:56 +0200

    previous commit was old file error. Updated

--- keymap.c
+++ keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------|   [   |              |   ]   |------+------+------+------+------+------|
   * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|              |-------|   /  |   N  |   M  |   ,  |   .  |   \  |
   * `-----------------------------------------/       /              \       \-----------------------------------------'
-  *                   | LAlt |BackSP|LOWER | /Enter  /                \Space  \  |RAISE | LGUI | CAPS |
+  *                   | LAlt |BackSP|LOWER | /Space  /                \Enter  \  |RAISE |  MOD | CAPS |
   *                   |      |      |      |/       /                  \       \ |      |      |      |
   *                   `----------------------------'                    '-------''--------------------'
   */
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,   \
     KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_MINS,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   \
     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,      KC_RBRC,   KC_LBRC, KC_SLSH,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSLS,   \
-                               KC_LALT,          MO(LOWER), KC_SPC,    KC_ENT,  TT(RAISE), KC_LGUI, KC_CAPS                               \
+                               KC_LALT, KC_BSPC, MO(LOWER), KC_SPC,    KC_ENT,  TT(RAISE), KC_LGUI, KC_CAPS                               \
   ),
 
   /* LOWER
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------|    [  |              |    ]  |------+------+------+------+------+------|
   * |LCTRL |      |   <  |   {  |   (  |   #  |-------|              |-------|   :  |   )  |   }  |   >  |      |      |
   * `-----------------------------------------/       /              \       \-----------------------------------------'
-  *                   |      |      |LOWER | / Space /                \ Enter \  |RAISE |BackSP|      |
+  *                   |      |BackSP|LOWER | / Space /                \ Enter \  |RAISE |      |      |
   *                   |      |      |      |/       /                  \       \ |      |      |      |
   *                   `----------------------------'                    '-------''--------------------'
   */
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,      \
     _______, KC_DLR,  KC_TILD, KC_EXLM, KC_QUES, KC_DQUO,                       KC_UNDS, KC_AMPR, KC_PIPE, KC_CIRC, KC_AT,   XXXXXXX,     \
     _______, XXXXXXX, KC_LT,   KC_LCBR, KC_LPRN, KC_HASH, _______,     _______, KC_COLN, KC_RPRN, KC_RCBR, KC_GT,   XXXXXXX, XXXXXXX,     \
-                               XXXXXXX, XXXXXXX, _______, _______,     _______, _______, _______, XXXXXXX                                 \
+                               XXXXXXX, _______, _______, _______,     _______, _______, XXXXXXX, XXXXXXX                                 \
   ),
 
   /* RAISE
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------|       |              |       |------+------+------+------+------+------|
   * |      |MOUSEL|MOUSEU|MOUSED|MOUSER| MUTE |-------|              |-------|   .  |   1  |   2  |   3  |   =  |      |
   * `-----------------------------------------/       /              \       \-----------------------------------------'
-  *                   |      |      |      | /MOUSE 1/                \MOUSE 2\  |QWERTY|   0  |Enter |
+  *                   |      |      |Enter | /MOUSE 1/                \MOUSE 2\  |QWERTY|  MOD |   0  |
   *                   |      |      |      |/       /                  \       \ |      |      |      |
   *                   `----------------------------'                    '-------''--------------------'
   */
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_PGUP, KC_UP,   KC_PGDN, KC_BRIU, KC_VOLU,                        KC_PAST,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, XXXXXXX,  \
     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_BRID, KC_VOLD,                        KC_PSLS,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, XXXXXXX,  \
     XXXXXXX, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_MUTE, XXXXXXX,     XXXXXXX,  KC_PDOT,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, XXXXXXX,  \
-                               XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1,     KC_BTN2,  TO(QWERTY),KC_P0,   KC_PENT \
+                               XXXXXXX, XXXXXXX, KC_PENT, KC_BTN1,     KC_BTN2,  TO(QWERTY),KC_LGUI, KC_P0                                \
   )
 };
 
